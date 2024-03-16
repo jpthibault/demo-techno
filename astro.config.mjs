2ef6306576dd0ebd0f8e3513c5df3d228e172b84
Jean-Philip Thibault <jeanphilip.thibault@gmail.com>
2024-03-16 12:22:48 -0400

    gh workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
   integrations: [mdx(), sitemap(), pagefind],
+  site: "https://jpthibault.github.io",
+  base: "/demo-techno",
 });
